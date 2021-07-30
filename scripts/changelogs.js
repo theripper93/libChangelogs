f3de73c515a93b1b481a02c17e797cffcd72941b
theripper93 <tsplab@gmail.com>
2021-07-30 03:04:06 +0200

    a

--- scripts/changelogs.js
+++ scripts/changelogs.js
@@ -3,13 +3,13 @@ class Changelogs extends FormApplication{
     constructor(){
         super();
         this.allChangelogs = {
-            "important" : {},
+            "critical" : {},
             "breaking" : {},
             "major" : {},
             "minor" : {},
         }
         this.changelogs = {
-            "important" : {},
+            "critical" : {},
             "breaking" : {},
             "major" : {},
             "minor" : {},
@@ -38,7 +38,7 @@ class Changelogs extends FormApplication{
         html.on("click", "#toggle-btn",(event) => {
             let $currTarget = $(event.currentTarget);
             $currTarget.toggleClass("fa-caret-right fa-caret-down");
-            $currTarget.closest(".chl-module-important").find(".chl-module-content").toggleClass("hidden");
+            $currTarget.closest(".chl-module-critical").find(".chl-module-content").toggleClass("hidden");
             $currTarget.closest(".chl-module-breaking").find(".chl-module-content").toggleClass("hidden");
             $currTarget.closest(".chl-module-major").find(".chl-module-content").toggleClass("hidden");
             $currTarget.closest(".chl-module-minor").find(".chl-module-content").toggleClass("hidden");
@@ -60,11 +60,12 @@ class Changelogs extends FormApplication{
     }
 
     filterAndSave(){
+        let warnLevel = game.settings.get("lib-changelogs", "warnLevel")
         let settingsToSave = this.readChangelogs;
         for(let [key,value] of Object.entries(this.allChangelogs)){
             for(let [moduleId, moduleData] of Object.entries(value)){
                 if(this.readChangelogs[moduleId]!=moduleData.version){
-                    this.changelogs[key][moduleId] = moduleData;
+                    if(this.warnLevel(key,warnLevel))this.changelogs[key][moduleId] = moduleData;
                     settingsToSave[moduleId] = moduleData.version;
                 }
             }
@@ -72,10 +73,26 @@ class Changelogs extends FormApplication{
         game.settings.set("lib-changelogs", "changelogs", settingsToSave);
     }
 
+    warnLevel(level,max){
+        if(max=="all") return true;
+        if(level=="critical") return true;
+        if(max=="major" && level != "minor") return true;
+        if(max=="breaking" && level != "minor" && level != "minor") return true;
+        return false;
+    }
+
+    get isNotEmpty(){
+        for(let [key,value] of Object.entries(this.changelogs)){
+            if(Object.entries(value).length>0) return true;
+        }
+        return false;
+    }
+
     render(...args) {
         if(args[2]=="all"){
             this.changelogs = this.allChangelogs;
         }
+        if(!this.isNotEmpty) return;
         super.render(...args);
     }
 
@@ -95,6 +112,30 @@ Hooks.once('init', function() {
         default: {},
       });
 
+      game.settings.register("lib-changelogs", "warnLevel", {
+        name: game.i18n.localize("lib-changelogs.settings.warnLevel.name"),
+        hint: game.i18n.localize("lib-changelogs.settings.warnLevel.hint"),
+        scope: "world",
+        config: true,
+        type: String,
+        choices: {
+            "all": game.i18n.localize("lib-changelogs.settings.warnLevel.all"),
+            "major": game.i18n.localize("lib-changelogs.settings.warnLevel.major"),
+            "breaking": game.i18n.localize("lib-changelogs.settings.warnLevel.breaking"),
+            "critical": game.i18n.localize("lib-changelogs.settings.warnLevel.critical"),
+        },
+        default: "all",
+      });
+
+      game.settings.register("lib-changelogs", "alwaysShow", {
+        name: game.i18n.localize("lib-changelogs.settings.alwaysShow.name"),
+        hint: game.i18n.localize("lib-changelogs.settings.alwaysShow.hint"),
+        scope: "world",
+        config: true,
+        type: Boolean,
+        default: false,
+      });
+
       libChangelogs = new Changelogs();
 
     Hooks.callAll('libChangelogsReady');
@@ -102,16 +143,38 @@ Hooks.once('init', function() {
 });
 
 Hooks.once('libChangelogsReady', function() {
-    libChangelogs.register("levels","TEEST","important")
-    libChangelogs.register("betterroofs","TEEST","important")
+    libChangelogs.register("levels","TEEST","critical")
+    libChangelogs.register("betterroofs","TEEST","critical")
     libChangelogs.register("combatbooster","TEEST","minor")
     libChangelogs.register("blastzone","TEEST","major")
     libChangelogs.register("fxmaster","TEEST","breaking")
-    libChangelogs.register("levels","TEEST","important")
-    libChangelogs.register("levels","TEEST","important")
+    libChangelogs.register("levels","TEEST","critical")
+    libChangelogs.register("levels","TEEST","critical")
 })
 
 Hooks.once('ready', function() {
     libChangelogs.filterAndSave();
-    libChangelogs.render(true,{},"all");
+    libChangelogs.render(true,{},
+        game.settings.get("lib-changelogs", "alwaysShow") ? "all" : ""
+        );
 });
+
+
+Hooks.on("renderSettingsConfig", function(form,html) {
+    let changelogsBtn = `<div class="form-group submenu">
+    <label></label>
+    <button type="button" id="lib-changelogs-menu">
+        <i class="fas fa-cog"></i>
+        <label>${game.i18n.localize("lib-changelogs.settings.showAll")}</label>
+    </button>
+    
+</div>`
+    html.find('input[name="lib-changelogs.warnLevel"]').closest(".form-group").before(changelogsBtn);
+
+    html.on("click", "#lib-changelogs-menu",(event) => {
+        event.preventDefault();
+        libChangelogs.render(true,{},"all");
+    })
+
+} )
+
